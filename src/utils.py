42faf9df1e73246c3a6429ef947f39c627b00823
tropicles <passigaurav383@gmail.com>
2025-06-16 16:55:48 +0530

    Training

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import dill
 import os
 import sys
-
+from sklearn.metrics import r2_score
 from src.exception import  CustomeException
 
 def save_object(file_path,obj):
@@ -15,3 +15,18 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomeException(e,sys)
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_pred_test=model.predict(X_test)
+            test_model_score=r2_score(y_test,y_pred_test)
+            report[list(models.keys())[i]]=test_model_score
+            return report
+        
+    except Exception as e:
+        raise CustomeException(e,sys)
+        
